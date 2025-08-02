772e06b6be2057ea6d57252dd32b1133eadb2c7b
JRobber66 <141540195+JRobber66@users.noreply.github.com>
2025-08-01 23:26:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
 import openai
+from flask_cors import CORS
+from flask import Flask, request, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
